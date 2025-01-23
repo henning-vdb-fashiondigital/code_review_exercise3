6d906142ffcdeb3938a086ecd0125fd80ddf3407
Henning von der Beck <henning.von-der-beck@fashion-digital.de>
2025-01-23 09:19:30 +0100

    Initial commit

---
+++ src/main/java/com/example/code_review_exercise3/CodeReviewExercise3Application.java
@@ -0,0 +1,13 @@
+package com.example.code_review_exercise3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeReviewExercise3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeReviewExercise3Application.class, args);
+	}
+
+}
